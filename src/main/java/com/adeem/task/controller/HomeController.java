821f188631d05a9f05e136fa4219f2228dbbfe93
aymededdine <aimededdine.touati@univ-constantine2.dz>
2023-11-13 15:39:32 +0100

    adding login-logout shifting functionality

---
+++ src/main/java/com/adeem/task/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.adeem.task.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.adeem.task.entity.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+	@GetMapping("/is-auth")
+	public ResponseEntity<?> taskForm(@AuthenticationPrincipal User user) {
+		var isAuth = false;
+		
+		if(user != null) {
+			isAuth = true;
+			return new ResponseEntity<>("isAuth", HttpStatus.ACCEPTED);
+		}
+		return new ResponseEntity<>("isAuth", HttpStatus.EXPECTATION_FAILED);
+
+	}
+
+}
